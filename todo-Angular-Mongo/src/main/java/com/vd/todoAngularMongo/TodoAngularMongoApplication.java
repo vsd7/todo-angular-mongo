5a07fca808e7eb4c41170bedd1dd104e8379d911
Vivek S Deshmukh <vsdrocks467@gmail.com>
2019-12-01 12:14:54 +0530

    First Commit

---
+++ todo-Angular-Mongo/src/main/java/com/vd/todoAngularMongo/TodoAngularMongoApplication.java
@@ -0,0 +1,13 @@
+package com.vd.todoAngularMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAngularMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAngularMongoApplication.class, args);
+	}
+
+}
